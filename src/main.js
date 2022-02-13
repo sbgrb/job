a0f9057f3409fd1f2d61fd30afea9f3d2634cd26
sbgrb <974487599@qq.com>
2022-02-13 22:13:20 +0800

    简历初始

---
+++ src/main.js
@@ -0,0 +1,38 @@
+// var myChart = echarts.init(document.getElementById('skill'));
+// var option = {
+//     tooltip: {},
+//     legend: {
+//         data: []
+//     },
+//     radar: {
+//         // shape: 'circle',
+//         name: {
+//             textStyle: {
+//                 color: '#fff',
+//                 backgroundColor: '#999',
+//                 borderRadius: 3,
+//                 padding: [3, 5]
+//             }
+//         },
+//         indicator: [
+//             { name: '静态网页', max: 100},
+//             { name: '沟通能力', max: 100},
+//             { name: '编程基础', max: 100},
+//             { name: 'Vue', max: 100},
+//             { name: '项目开发', max: 100},
+//             { name: '学习能力', max: 100}
+//         ]
+//     },
+//     series: [{
+//         name: '',
+//         type: 'radar',
+//         // areaStyle: {normal: {}},
+//         data: [
+//             {
+//                 value: [90,80,85,85,70,90],
+//                 name: '个人能力自评'
+//             },
+//         ]
+//     }]
+// };
+// myChart.setOption(option);
